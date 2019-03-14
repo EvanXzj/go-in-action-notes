7cf4c5afc0668a875fd326938d9a6ca218f8c2e7
EvanXzj <2686465170@qq.com>
2019-03-14 11:20:06 +0800

    listing35 done

---
+++ chapter05/listing35/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	var b bytes.Buffer
+
+	//b.Write([]byte("Hello"))
+	//fmt.Fprintf(&b, " World!")
+	//
+	//io.Copy(os.Stdout, &b)
+
+	// Write a string to the buffer.
+	b.Write([]byte("Hello"))
+
+	// Use Fprintf to concatenate a string to the Buffer.
+	fmt.Fprintf(&b, "World!")
+
+	// Write the content of the Buffer to stdout.
+	io.Copy(os.Stdout, &b)
+}
