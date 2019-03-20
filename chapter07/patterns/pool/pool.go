7355b00278cc3b2481d9db01178048e5a09d19de
EvanXzj <2686465170@qq.com>
2019-03-20 14:29:03 +0800

    chapter7 pool pattern done

---
+++ chapter07/patterns/pool/pool.go
@@ -0,0 +1,81 @@
+// Example provided with help from Fatih Arslan and Gabriel Aszalos.
+// Package pool manages a user defined set of resources.
+package pool
+
+import (
+	"errors"
+	"io"
+	"log"
+	"sync"
+)
+
+type Pool struct {
+	m         sync.Mutex
+	resources chan io.Closer
+	factory   func() (io.Closer, error)
+	closed    bool
+}
+
+var ErrPoolClosed = errors.New("pool has been closed")
+
+func New(fn func() (io.Closer, error), size uint) (*Pool, error) {
+	if size <= 0 {
+		return nil, errors.New("size value to small")
+	}
+
+	return &Pool{
+		factory:   fn,
+		resources: make(chan io.Closer, size),
+	}, nil
+}
+
+func (p *Pool) Acquire() (io.Closer, error) {
+	select {
+	case r, ok := <-p.resources:
+		log.Println("Acquire:", "Shared Resource")
+		if !ok {
+			return nil, ErrPoolClosed
+		}
+		return r, nil
+	default:
+		log.Println("Acquire:", "New Resource")
+		return p.factory()
+	}
+}
+
+func (p *Pool) Release(r io.Closer) {
+	p.m.Lock()
+	defer p.m.Unlock()
+
+	if p.closed {
+		r.Close()
+		return
+	}
+
+	select {
+	case p.resources <- r:
+		log.Println("Release:", "In Queue")
+	default:
+		log.Println("Release:", "Closing")
+		r.Close()
+	}
+}
+
+func (p *Pool) Close() {
+	p.m.Lock()
+	defer p.m.Unlock()
+
+	if p.closed {
+		return
+	}
+
+	p.closed = true
+
+	// 在清空通道里的资源之前，将通道关闭
+	// 如果不这样做，会发生死锁
+	close(p.resources)
+
+	for r := range p.resources {
+		r.Close()
+	}
+}
